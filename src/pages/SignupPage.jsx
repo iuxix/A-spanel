958e34005d29eefd761464d4641138de33d8ea74
iuxix <117330015+iuxix@users.noreply.github.com>
2025-07-19 19:10:12 +0530

    Create SignupPage.jsx

---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SignupForm from "../components/SignupForm";
+export default function SignupPage() {
+  return (
+    <div className="auth-bg">
+      <div className="login-container">
+        <SignupForm />
+      </div>
+    </div>
+  );
+}
