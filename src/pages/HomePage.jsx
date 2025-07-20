6cc273c2ec3e8b432c017baf7b645b29ca472b86
iuxix <117330015+iuxix@users.noreply.github.com>
2025-07-20 07:36:22 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,24 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import HeroSection from "../components/HeroSection";
-import StatsCards from "../components/StatsCards";
-import ServiceIcons from "../components/ServiceIcons";
-
+import { Link } from "react-router-dom";
 export default function HomePage() {
   return (
-    <>
-      <Navbar />
-      <main>
-        <HeroSection />
-        <StatsCards />
-        <section>
-          <h2 style={{ textAlign: "center", margin: "30px 0 12px" }}>
-            Top SMM Services We Provide
-          </h2>
-          <ServiceIcons />
-        </section>
-      </main>
-    </>
+    <div className="lp-main">
+      <div className="lp-content">
+        <div className="lp-title">Grow Faster with <span style={{color:'#0da4e1'}}>fastsmmpanel</span> 🚀</div>
+        <div className="lp-desc">
+          The #1 SMM panel.<br/>
+          Real followers, fast views, full analytics, secure wallet, instant orders.<br/>
+          <b style={{color:'#18a850'}}>Start now. Make your growth unstoppable!</b>
+        </div>
+        <div className="lp-btns">
+          <Link to="/login" className="lp-btn signin">Sign In</Link>
+          <Link to="/signup" className="lp-btn signup">Sign Up</Link>
+          <Link to="/admin" className="lp-btn admin">Admin</Link>
+        </div>
+      </div>
+      <div className="lp-imgbox">
+        <img src="/logo.png" className="lp-img" alt="Panel Logo" />
+      </div>
+    </div>
   );
 }
