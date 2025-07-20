e92c4ca202370d9de28a1499903c2f8c404a61b5
iuxix <117330015+iuxix@users.noreply.github.com>
2025-07-20 09:41:14 +0530

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import LoginForm from "../components/LoginForm";
+import Navbar from "../components/Navbar";
 export default function LoginPage() {
   return (
     <div className="auth-bg">
+      <Navbar />
       <div className="auth-container">
         <LoginForm />
       </div>
