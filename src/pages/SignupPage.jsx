f9294cb83956b4102e22af412a772a4651a30241
iuxix <117330015+iuxix@users.noreply.github.com>
2025-07-20 15:56:04 +0530

    Update SignupPage.jsx

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import SignupForm from "../components/SignupForm";
+import Navbar from "../components/Navbar";
+
 export default function SignupPage() {
   return (
     <div className="auth-bg">
-      <div className="login-container">
+      <Navbar />
+      <div className="auth-container">
         <SignupForm />
       </div>
     </div>
