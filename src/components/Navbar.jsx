fd9a54287e945a823e1f355b7216d8395c2a74e8
iuxix <117330015+iuxix@users.noreply.github.com>
2025-07-20 07:37:26 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      <span className="brand">LuciXFire Panel</span>
-      <div className="nav-links">
-        <Link to="/login">Sign In</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/admin">Admin</Link>
-      </div>
-    </nav>
+    <div className="navbar">
+      <span className="brand">fastsmmpanel</span>
+    </div>
   );
 }
